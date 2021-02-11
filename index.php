f416dc60c071347e9122a1f1d84b11c25903572b
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2021-02-11 16:30:50 +0100

    projet en cour

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/940412168c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/animation.css">
+    <title>index</title>
+</head>
+<body>
+<?php
+    include "./pages/partials/header.php";
+?>
+
+    <div id="container">
+
+    </div>
+    <footer>
+
+    </footer>
+
+
+
+
+
+
+
+
+
+    <script src="JS/script.js"></script>
+</body>
+</html>
