209a28a2b68fe72c96274a2d942c493c5b0479d9
BessonMikael <besson.mikael04@gmail.com>
2021-02-12 09:50:23 +0100

    changement de l'animation header

--- pages/partials/header.php
+++ pages/partials/header.php
@@ -4,8 +4,8 @@
 
 <header>
     <a class="slideInLeft3" id="menu" href="../menu.php"><i class="fas fa-bars"></i>  Menu</a>
-    <div class="slideInLeft2" id="barreRecherche">
+    <div class="fondu" id="barreRecherche">
         <input id="recherche" name="recherche" type="text" placeholder="recherche ">
     </div>
-    <a class="slideInLeft1" id="signIt" href="../adhesion.php">Sign In  <i class="fas fa-user-plus"></i></a>
+    <a class="slideInRight1" id="signIt" href="../adhesion.php">Sign In  <i class="fas fa-user-plus"></i></a>
 </header>
