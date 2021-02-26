0364e4a00465ad2025668a80f3db7b520127346c
BessonMikael <besson.mikael04@gmail.com>
2021-02-26 11:46:54 +0100

    ajout des descriptif des films

--- pages/partials/bodyIndex.php
+++ pages/partials/bodyIndex.php
@@ -62,61 +62,210 @@
 <div class="texte-container">
     <div class="texte-animated">
         <span>la Guerre Des Etoiles</span>
-        <p>
+        <p class="intro">
             19 octobre 1977 / 2h 01min / Science fiction, Aventure<br>
             De George Lucas<br>
             Avec Mark Hamill, Harrison Ford, Carrie Fisher<br>
         </p>
+        <br>
+        <div class="resume">
+            <p>
+                Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre <br>
+                l'Empire galactique et l'Alliance rebelle. Capturée par les troupes de choc de l'Empereur  <br>
+                menées par le sombre et impitoyable Dark Vador, la princesse Leia Organa dissimule les plans  <br>
+                de l'Etoile Noire, une station spatiale invulnérable, à son droïde R2-D2 avec pour mission  <br>
+                de les remettre au Jedi Obi-Wan Kenobi. Accompagné de son fidèle compagnon,  <br>
+                le droïde de protocole C-3PO, R2-D2 s'échoue sur la planète Tatooine et termine sa quête chez  <br>
+                le jeune Luke Skywalker. Rêvant de devenir pilote mais confiné aux travaux de la ferme,  <br>
+                ce dernier se lance à la recherche de ce mystérieux Obi-Wan Kenobi, devenu ermite au coeur  <br>
+                des montagnes désertiques de Tatooine...
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>L'Empire contre-attaque</span>
+        <p class="intro">
+            20 août 1980 / 2h 04min / Science fiction, Aventure<br>
+            De Irvin Kershner <br>
+            Avec Mark Hamill, Harrison Ford, Carrie Fisher <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                Malgré la destruction de l'Etoile Noire, l'Empire maintient son emprise sur la galaxie,  <br>
+                et poursuit sans relâche sa lutte contre l'Alliance rebelle. Basés sur la planète glacée de Hoth,  <br>
+                les rebelles essuient un assaut des troupes impériales. Parvenus à s'échapper,  <br>
+                la princesse Leia, Han Solo, Chewbacca et C-3P0 se dirigent vers Bespin,  <br>
+                la cité des nuages gouvernée par Lando Calrissian, ancien compagnon de Han.  <br>
+                Suivant les instructions d'Obi-Wan Kenobi, Luke Skywalker se rend quant à lui vers le système de Dagobah, <br>
+                planète marécageuse où il doit recevoir l'enseignement du dernier maître Jedi, Yoda.  <br>
+                Apprenant l'arrestation de ses compagnons par les stormtroopers de Dark Vador après la trahison de Lando, <br>
+                Luke décide d'interrompre son entraînement pour porter secours à ses amis et affronter le sombre seigneur Sith...
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>Le Retour du Jedi</span>
-
+        <p class="intro">
+            19 octobre 1983 / 2h 13min / Science fiction, Aventure<br>
+            De Richard Marquand  <br>
+            Avec Mark Hamill, Harrison Ford, Carrie Fisher <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                L'Empire galactique est plus puissant que jamais : la construction de la nouvelle arme, l'Etoile de la Mort,<br>
+                menace l'univers tout entier... Arrêté après la trahison de Lando Calrissian,<br>
+                Han Solo est remis à l'ignoble contrebandier Jabba Le Hutt par le chasseur de primes Boba Fett.<br>
+                Après l'échec d'une première tentative d'évasion menée par la princesse Leia, également arrêtée par Jabba,<br>
+                Luke Skywalker et Lando parviennent à libérer leurs amis.
+                Han, Leia, Chewbacca, C-3PO et Luke, devenu un Jedi, s'envolent dès lors pour une mission d'extrême <br>
+                importance sur la lune forestière d'Endor, afin de détruire le générateur du bouclier de l'Etoile de la Mort<br>
+                et permettre une attaque des pilotes de l'Alliance rebelle. Conscient d'être un danger pour ses compagnons,<br>
+                Luke préfère se rendre aux mains de Dark Vador, son père et ancien Jedi passé du côté obscur de la Force.
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>La Menace fantôme</span>
-
+        <p class="intro">
+            13 octobre 1999 / 2h 13min / Science fiction, Aventure, Fantastique<br>
+            De George Lucas   <br>
+            Avec Liam Neeson, Ewan McGregor, Natalie Portman  <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                Avant de devenir un célèbre chevalier Jedi, et bien avant de se révéler l’âme la plus noire de la galaxie,<br>
+                Anakin Skywalker est un jeune esclave sur la planète Tatooine. La Force est déjà puissante en lui et <br>
+                il est un remarquable pilote de Podracer. Le maître Jedi Qui-Gon Jinn le découvre et entrevoit alors <br>
+                son immense potentiel.
+                Pendant ce temps, l’armée de droïdes de l’insatiable Fédération du Commerce a envahi Naboo, <br>
+                une planète pacifique, dans le cadre d’un plan secret des Sith visant à accroître leur pouvoir. <br>
+                Pour défendre la reine de Naboo, Amidala, les chevaliers Jedi vont devoir affronter <br>
+                le redoutable Seigneur Sith, Dark Maul
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>L'Attaque des clones</span>
-
+        <p class="intro">
+            17 mai 2002 / Science fiction, Aventure, Fantastique<br>
+            De George Lucas   <br>
+            Avec Ewan McGregor, Natalie Portman, Hayden Christensen  <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                La sénatrice Padmé Amidala échappe de justesse à un attentat. Le Padawan Anakin Skywalker est chargé <br>
+                de sa protection. Son maître, Obi-Wan Kenobi, enquête sur cette tentative de meurtre et découvre <br>
+                la constitution d'une mystérieuse armée de clones.
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>La Revanche des Sith</span>
-
-
+        <p class="intro">
+            18 mai 2005 / Science fiction, Aventure, Fantastique<br>
+            De George Lucas   <br>
+            Avec Ewan McGregor, Natalie Portman, Hayden Christensen  <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                La Guerre des Clones fait rage. Une franche hostilité oppose désormais le Chancelier Palpatine <br>
+                au Conseil Jedi. Anakin Skywalker, jeune Chevalier Jedi pris entre deux feux,<br>
+                hésite sur la conduite à tenir...
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>Le Réveil de la Force</span>
-
+        <p class="intro">
+            16 décembre 2015 / Science fiction, Aventure, Fantastique<br>
+            De J.J. Abrams    <br>
+            Avec Daisy Ridley, John Boyega, Adam Driver  <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                Dans une galaxie lointaine, très lointaine, un nouvel épisode de la saga "Star Wars",<br>
+                30 ans après les événements du "Retour du Jedi".
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>Les Derniers Jedi</span>
-
+        <p class="intro">
+            13 décembre 2017 / Science fiction, Aventure, Fantastique<br>
+            De Rian Johnson     <br>
+            Avec Daisy Ridley, John Boyega, Oscar Isaac   <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                Les héros du Réveil de la force rejoignent les figures légendaires de la galaxie dans une aventure<br>
+                épique qui révèle des secrets ancestraux sur la Force et entraîne de surprenantes révélations sur le passé…
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>L'Ascension des Skywalker</span>
-
+        <p class="intro">
+            18 décembre 2019 / 2h 22min / Science fiction, Aventure<br>
+            De J.J. Abrams     <br>
+            Avec Daisy Ridley, Adam Driver, Oscar Isaac   <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette bataille épique pour la liberté.
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>Rogue One</span>
-
+        <p class="intro">
+            14 décembre 2016 / 2h 14min / Science fiction, Aventure, Action<br>
+            De Gareth Edwards<br>
+            Avec Felicity Jones, Diego Luna, Ben Mendelsohn <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                Situé entre les épisodes III et IV de la saga Star Wars, le film nous entraîne aux côtés d’individus <br>
+                ordinaires qui, pour rester fidèles à leurs valeurs, vont tenter l’impossible au péril de leur vie. <br>
+                Ils n’avaient pas prévu de devenir des héros, mais dans une époque de plus en plus sombre, <br>
+                ils vont devoir dérober les plans de l’Étoile de la Mort, l’arme de destruction ultime de l’Empire.
+            </p>
+        </div>
     </div>
 
     <div class="texte-animated">
         <span>Solo</span>
-
+        <p class="intro">
+            23 mai 2018 / 2h 15min / Science fiction, Fantastique<br>
+            De Ron Howard <br>
+            Avec Alden Ehrenreich, Woody Harrelson, Emilia Clarke <br>
+        </p>
+        <br>
+        <div class="resume">
+            <p>
+                Embarquez à bord du Faucon Millenium et partez à l’aventure en compagnie du plus célèbre vaurien <br>
+                de la galaxie. Au cours de périlleuses aventures dans les bas-fonds d’un monde criminel, <br>
+                Han Solo va faire la connaissance de son imposant futur copilote Chewbacca et croiser la route <br>
+                du charmant escroc Lando Calrissian… Ce voyage initiatique révèlera la personnalité d’un des héros <br>
+                les plus marquants de la saga Star Wars.
+            </p>
     </div>
 </div>
 </body>
